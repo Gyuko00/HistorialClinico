6c6ffffa620fce61da4c8ca64fd608ee2073a4d0
Jd_ <135854874+Gyuko00@users.noreply.github.com>
2024-04-29 09:17:33 -0500

    conexion.js

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-let conexion = "mongodb+srv://UTS:uts2024@uts.ccyqodk.mongodb.net/Prueba2024E192?retryWrites=true&w=majority&appName=UTS";
+let conexion = "mongodb+srv://UTS:uts2024@uts.ccyqodk.mongodb.net/Dev2024E192?retryWrites=true&w=majority&appName=UTS";
 
 mongoose.connect(conexion)
     .then(event => console.log("Conectado a Mongo"))
     .catch(error => console.log(error));
 
 module.exports = mongoose;
+
 
 
